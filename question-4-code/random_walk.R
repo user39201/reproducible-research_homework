da62dcb6bf8e19b7bfc15883950446dd6f460dc9
Jon Harper <jonathan.harper@st-hildas.ox.ac.uk>
2023-12-05 16:16:13 +0000

    Q4 code edit2

--- question-4-code/random_walk.R
+++ question-4-code/random_walk.R
@@ -4,31 +4,6 @@ install.packages("gridExtra")
 library(ggplot2)
 library(gridExtra)
 
-random_walk  <- function (n_steps) {
-  
-  df <- data.frame(x = rep(NA, n_steps), y = rep(NA, n_steps), time = 1:n_steps)
-  
-  df[1,] <- c(0,0,1)
-  
-  for (i in 2:n_steps) {
-    
-    h <- 0.25
-    
-    angle <- runif(1, min = 0, max = 2*pi)
-    
-    df[i,1] <- df[i-1,1] + cos(angle)*h
-    
-    df[i,2] <- df[i-1,2] + sin(angle)*h
-    
-    df[i,3] <- i
-    
-  }
-  
-  return(df)
-  
-}
-
-
 random_walk <- function(n_steps, seed = seed) {
   
   set.seed(seed)
